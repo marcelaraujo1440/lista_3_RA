2d28ff70c5e8ca54e933fe12b3440c3b53992bc1
marcelaraujo1440 <162705599+marcelaraujo1440@users.noreply.github.com>
2024-04-11 08:03:10 -0300

    Add files via upload

---
+++ exfor10.py
@@ -0,0 +1,3 @@
+lista= [2,4,7,2,3,3,1,0,2,6]
+for i in range(0,1):
+    print(F"{lista.count(2)}")
---
+++ exfor7.py
@@ -0,0 +1,8 @@
+#precisa simplificar ainda 
+soma= 0
+for num in range(2,101, 2):
+    soma+=num
+    print(soma)
+    
+
+    
